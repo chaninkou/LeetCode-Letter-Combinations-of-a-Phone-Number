5e9d5ee92aae8eea66dd8c63c16fe4014eb10a66
chaninkou <michaelkoulove@yahoo.com>
2019-12-26 01:12:49 -0800

    Great review on linked list, similar to queue, unique patterns.

--- src/phoneNumber/FindAllPossibleLetterCombinationsFunction.java
+++ src/phoneNumber/FindAllPossibleLetterCombinationsFunction.java
@@ -30,6 +30,16 @@ public class FindAllPossibleLetterCombinationsFunction {
             // If there is empty string (thats why we add empty string at start) so add one letter, 
             // then there is only one letter in the queue add one letter again, 
             // then two letter in the queue  and so on
+            
+            // Example: a->b->c , this is after first i iteration
+            // then: peek() will check the length to be 1 == 1
+            // so remove a, use a to add
+            // so, b -> c -> ad -> ae -> af
+            // then: peek() will check the length to be 1 == 1
+            // so c -> ad -> ae -> af -> c -> bd -> be -> bf 
+            // then: peek() will check the length to be 1 == 1
+            // so c -> ad -> ae -> af -> c -> bd -> be -> bf -> cd -> ce -> cf 
+            
             while(result.peek().length() == i){
                 // remove() in LinkedList is like the pop() value from the queue, first it will be "", then one letter, then two letter, and so on
                 String permutation = result.remove();
